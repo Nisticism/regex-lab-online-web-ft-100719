84e61ccac9c6af2d1b44efd29b9fd11a2d95207f
Hans Foster <fosterhans@gmail.com>
2019-12-02 19:49:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,36 @@
 def starts_with_a_vowel?(word)
-
+  all_vowels = []
+  all_vowels = word.scan(/[aeiouAEIOU]/)
+  if all_vowels [0] == word[0]
+    true
+  else 
+    false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un/)
 end
 
 def words_five_letters_long(text)
-
+  new_array = text.split(" ")
+  new_array.grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  a = true
+  if text[0] == /[A-Z]/
+    a = true
+  else 
+    a = false 
+  end
+  
+  if text[text.length - 1] == /[.,!?]/
+    a = true
+  else 
+    a = false 
+  end
+  a
 end
 
 def valid_phone_number?(phone)
