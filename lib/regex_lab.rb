68f6ca45eb8efdc136ca0185fc7733f69cf6e382
Hans Foster <fosterhans@gmail.com>
2019-12-02 20:30:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un/)
+  text.scan(/un(\w+)ing/)
 end
 
 def words_five_letters_long(text)
@@ -34,5 +34,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\d - \d - \d/)
 end
