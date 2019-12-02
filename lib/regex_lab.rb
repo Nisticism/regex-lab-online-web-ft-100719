36d20b312ded54b4e505fae5b923868b4de4dfd9
Hans Foster <fosterhans@gmail.com>
2019-12-02 21:23:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   text_array = text.split(" ")
   new_array = []
   text_array.each do |word|
-    if word[0-1].match(/\Aun/)
+    if word[0-1].match(/\A[un]/)
       new_array += word
     end
   end
