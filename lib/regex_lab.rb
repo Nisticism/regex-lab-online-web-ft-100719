1624456158dfda8fe963881badcc6139dee82126
Hans Foster <fosterhans@gmail.com>
2019-12-02 21:43:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  # text.scan(/\w[\Aun\Zing]/)
+  text.scan(/\Aun/)
   
   text_array = text.split(" ")
   new_array = []
